f2e5141ce200c670892424c8080a3cb3243525e8
Tolu Joel <toluisjoel@gmail.com>
2024-01-17 20:35:38 +0100

    update: Add password validation for UserSerializer

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -15,6 +15,18 @@ class UserSerializer(serializers.ModelSerializer):
             'password2'
         ]
 
+    def validate(self, attrs):
+        """
+        Validate the password and password2 fields to ensure they match.
+        """
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+
+        if password != password2:
+            raise serializers.ValidationError('Passwords must match')
+
+        return attrs
+
     def create(self, validated_data):
         validated_data.pop('password2')
         return get_user_model().objects.create_user(**validated_data)
