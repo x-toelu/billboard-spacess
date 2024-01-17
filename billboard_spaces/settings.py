d449e13968c938ebd19bae783d1f58375691013f
Tolu Joel <toluisjoel@gmail.com>
2024-01-17 22:30:20 +0100

    fix: Load environment variables from .env file

--- billboard_spaces/settings.py
+++ billboard_spaces/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -21,6 +22,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
