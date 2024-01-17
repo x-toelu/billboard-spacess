4e3fff53b250eded9b25f2fa5bba1134a0862023
Tolu Joel <toluisjoel@gmail.com>
2024-01-17 19:00:15 +0100

    refactor: Update CustomUser model Remove 'username' field from CustomUser Update email field with translation and unique constraint Adjust 'REQUIRED_FIELDS' to an empty list

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,15 +5,15 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "username", "password1", "password2"),
-            },
-        ),
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
+    search_fields = ('email',)
+    ordering = ('email',)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
@@ -11,7 +12,12 @@ class CustomUser(AbstractUser):
         editable=False,
         unique=True
     )
-    email = models.EmailField(max_length=100, unique=True)
+
+    username = None
+    email = models.EmailField(_("email address"), max_length=100, unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
