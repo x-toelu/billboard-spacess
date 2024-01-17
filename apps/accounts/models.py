bda858571d0221603daa96e441bfc5e12d56ae14
Tolu Joel <toluisjoel@gmail.com>
2024-01-17 19:03:17 +0100

    refactor: Create user model and manager for createsuperuser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,25 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -15,6 +32,8 @@ class CustomUser(AbstractUser):
 
     username = None
     email = models.EmailField(_("email address"), max_length=100, unique=True)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
