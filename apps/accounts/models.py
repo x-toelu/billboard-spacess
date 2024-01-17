9c1e5c7b175f7faa314c6c33a9751043d1e68a71
Tolu Joel <toluisjoel@gmail.com>
2024-01-17 16:46:47 +0100

    update: Change custom user field to UUID

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,16 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
     email = models.EmailField(max_length=100, unique=True)
 
     USERNAME_FIELD = 'email'
