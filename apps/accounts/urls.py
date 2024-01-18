bb471f6ddf4a1c7e94061d5ccbd8d3e0343a4464
Tolu Joel <toluisjoel@gmail.com>
2024-01-18 14:10:37 +0100

    feat: Add endpoint to generate user token

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import UserCreationView
 
 urlpatterns = [
-    path('', UserCreationView.as_view(), name='register'),
+    path('create/', UserCreationView.as_view(), name='create-user'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
 ]
