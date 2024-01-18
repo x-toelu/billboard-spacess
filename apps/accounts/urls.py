5e3d3dfa706da36c37f4f302f73a0ad6e0a944bb
Tolu Joel <toluisjoel@gmail.com>
2024-01-18 14:33:27 +0100

    refactor: sort imports and format code

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,8 +8,8 @@ from rest_framework_simplejwt.views import (
 from .views import UserCreationView
 
 urlpatterns = [
-    path('create/', UserCreationView.as_view(), name='create-user'),
-    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('create/', UserCreationView.as_view(), name='users_create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
