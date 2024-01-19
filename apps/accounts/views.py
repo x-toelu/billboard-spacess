dfce4a9c0f2bb8621198ab24c73071c72d594e27
Tolu Joel <toluisjoel@gmail.com>
2024-01-19 15:18:17 +0100

    feat: Add update profile view and endpoint

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,25 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
-from .serializers import UserSerializer
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.views import Response, status
+
+from .serializers import UpdateProfileSerializer, UserSerializer
 
 
 class UserCreationView(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class UpdateProfileView(UpdateAPIView):
+    serializer_class = UpdateProfileSerializer
+    queryset = get_user_model().objects.all()
+    allowed_methods = ['PATCH']
+
+    def put(self, request, *args, **kwargs):
+        error_data = {
+            "errors": ["Method 'PUT' not allowed."],
+            "message": "MethodNotAllowed",
+        }
+        return Response(error_data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
