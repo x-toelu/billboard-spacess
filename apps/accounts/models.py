2825698932580366d70e441604a0cd90e6c922d5
Tolu Joel <toluisjoel@gmail.com>
2024-01-19 09:23:17 +0100

    feat: Update custom user model with new fields     - Add field display_name to customuser     - Add field full_name to customuser     - Add field phone_number to customuser     - Add field state_of_residence to customuser     - Add field user_field to customuser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -23,15 +23,43 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
+    USER_CHOICES = (
+        ('billboard_owner', 'Billboard Owner'),
+        ('state_agent', 'State Agent'),
+        ('advertising_agent', 'Advertising Agent'),
+        ('business_owner', 'Business Owner'),
+    )
+    NIGERIAN_STATES = [
+        ('abia', 'Abia'), ('adamawa', 'Adamawa'), ('akwa_ibom', 'Akwa Ibom'),
+        ('anambra', 'Anambra'), ('bauchi', 'Bauchi'), ('bayelsa', 'Bayelsa'),
+        ('benue', 'Benue'), ('borno', 'Borno'), ('cross_river', 'Cross River'),
+        ('delta', 'Delta'), ('ebonyi', 'Ebonyi'), ('edo', 'Edo'), ('ekiti', 'Ekiti'),
+        ('enugu', 'Enugu'), ('gombe', 'Gombe'), ('imo', 'Imo'), ('jigawa', 'Jigawa'),
+        ('kaduna', 'Kaduna'), ('kano', 'Kano'), ('katsina', 'Katsina'),
+        ('kebbi', 'Kebbi'), ('kogi', 'Kogi'), ('kwara', 'Kwara'), ('lagos', 'Lagos'),
+        ('nasarawa', 'Nasarawa'), ('niger', 'Niger'), ('ogun', 'Ogun'),
+        ('ondo', 'Ondo'), ('osun', 'Osun'), ('oyo', 'Oyo'), ('plateau', 'Plateau'),
+        ('rivers', 'Rivers'), ('sokoto', 'Sokoto'), ('taraba', 'Taraba'),
+        ('yobe', 'Yobe'), ('zamfara', 'Zamfara'), ('federal_capital_territory', 'FCT')
+    ]
+
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False,
         unique=True
     )
-
     username = None
     email = models.EmailField(_("email address"), max_length=100, unique=True)
+    user_field = models.CharField(
+        max_length=20, choices=USER_CHOICES, blank=True, null=True
+    )
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    state_of_residence = models.CharField(
+        max_length=255, choices=NIGERIAN_STATES, blank=True, null=True
+    )
+    display_name = models.CharField(max_length=255, blank=True, null=True)
 
     objects = CustomUserManager()
 
