3a3f00bc20348437b8d6c2e3cffcaa14411148b1
Tolu Joel <toluisjoel@gmail.com>
2024-01-20 16:15:24 +0100

    feat: Add view and enpoint to reset(change) a user's password

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny
@@ -13,6 +14,7 @@ from rest_framework.views import Response, status
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (
     PasswordResetRequestSerializer,
+    PasswordResetSerializer,
     UpdateProfileSerializer,
     UserSerializer,
 )
@@ -66,3 +68,29 @@ class PasswordResetRequestView(CreateAPIView):
             )
 
         return Response({'detail': 'Password reset email sent.'})
+
+
+class PasswordResetConfirmView(CreateAPIView):
+    serializer_class = PasswordResetSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = PasswordResetSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        uidb64 = serializer.validated_data.get('uid')
+        token = serializer.validated_data.get('token')
+        password = serializer.validated_data.get('password')
+
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = get_user_model().objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist) as e:
+            user = None
+
+        # change password if token is valid and not expired.
+        if user and default_token_generator.check_token(user, token):
+            user.set_password(password)
+            user.save()
+            return Response({'detail': 'Password reset successful.'})
+        else:
+            return Response({'detail': 'Invalid reset link.'}, status=status.HTTP_400_BAD_REQUEST)
