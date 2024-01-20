df285729ed2dddda1cfe81d4300db2e35e6f127e
Tolu Joel <toluisjoel@gmail.com>
2024-01-20 17:04:14 +0100

    chore: Change `token/` endpoint to `login/`. My frontend guy complained

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
         UpdateProfileView.as_view(),
         name='update_profile'
     ),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
