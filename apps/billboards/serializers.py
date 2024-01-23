c51ee9a5fe4f9519ebea23f2a87ce6d87a85641a
Tolu Joel <toluisjoel@gmail.com>
2024-01-23 23:17:34 +0100

    feat: Add new fields to `BillboardSerializer`

--- apps/billboards/serializers.py
+++ apps/billboards/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
 from .models import Billboard
+from dateutil.relativedelta import relativedelta
 
 
 class BillboardSerializer(serializers.ModelSerializer):
+    available_date_in_days = serializers.SerializerMethodField()
+    available_date_in_weeks = serializers.SerializerMethodField()
+    available_date_in_months = serializers.SerializerMethodField()
+
     class Meta:
         model = Billboard
         fields = [
@@ -12,6 +17,23 @@ class BillboardSerializer(serializers.ModelSerializer):
             'size',
             'location',
             'available_date',
+            'available_date_in_days',
+            'available_date_in_weeks',
+            'available_date_in_months',
             'target_audience',
             'created_at',
         ]
+
+    def get_available_date_in_days(self, obj):
+        print(obj.image, 'kool')
+        days = (obj.available_date_to - obj.available_date_from).days + 1
+        return  f"{days} days"
+
+    def get_available_date_in_weeks(self, obj):
+        weeks = (obj.available_date_to - obj.available_date_from).days // 7
+        return  f"{weeks} weeks"
+
+    def get_available_date_in_months(self, obj):
+        months = relativedelta(obj.available_date_to, obj.available_date_from).months
+        return  f"{months} months"
+
