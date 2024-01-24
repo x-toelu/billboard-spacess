75faf8a33c724a6f98167885cf6b05247f1d7aad
Tolu Joel <toluisjoel@gmail.com>
2024-01-24 12:16:19 +0100

    chore: Update `build.sh`

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --no-input --email admin@admin.com
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@example.com', 'password')" | python manage.py shell
+
