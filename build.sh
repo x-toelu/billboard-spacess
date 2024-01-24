59bc7688805294e3c30862ccdec8014a77dd6e2f
Tolu Joel <toluisjoel@gmail.com>
2024-01-24 11:17:14 +0100

    chore: Update `build.sh`

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 python -m pip install -r requirements.txt
 
-python manage.py createsuperuser --no-input --email $DJANGO_ADMIN_EMAIL
+python manage.py createsuperuser --no-input --email admin@admin.com
 
 python manage.py collectstatic --no-input
 python manage.py migrate
