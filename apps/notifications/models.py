ec9f78ece500e9035ef5e8427462ce06961703f5
Tolu Joel <toluisjoel@gmail.com>
2024-01-26 14:51:17 +0100

    update: Add `is_read` field and `mark_as_read` function to Notification model

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -15,5 +15,15 @@ class Notification(models.Model):
         related_name='notifications'
     )
     type = models.CharField(max_length=15, choices=NOTIF_TYPES)
-    body = models.TextField()
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    def mark_as_read(self):
+        self.is_read = True
+        self.save()
+
