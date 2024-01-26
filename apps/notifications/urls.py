1758bbbec992f785a07e60c201d150c3be51d0a9
Tolu Joel <toluisjoel@gmail.com>
2024-01-26 15:58:59 +0100

    feat: Implement endpoint to get notif detail and mark as read

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import NotificationListAPIView
+from .views import NotificationDetailAPIView, NotificationListAPIView
 
 urlpatterns = [
     path('', NotificationListAPIView.as_view(), name='notification-list'),
+    path('read/<int:pk>/', NotificationDetailAPIView.as_view(), name='notification-detail'),
 ]
--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,5 +1,5 @@
-# yourapp/views.py
-from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
 
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -8,6 +8,22 @@ from .serializers import NotificationSerializer
 class NotificationListAPIView(ListAPIView):
     serializer_class = NotificationSerializer
     pagination_class = None
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
+
+
+class NotificationDetailAPIView(RetrieveAPIView):
+    """
+    Mark a single notification as read.
+    """
+    serializer_class = NotificationSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.mark_as_read()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user)
