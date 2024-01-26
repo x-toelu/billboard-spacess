5a98fef4c60f0b54c3d89953f1cbe43040ce7519
Tolu Joel <toluisjoel@gmail.com>
2024-01-26 14:46:19 +0100

    feat: Add `Notification` model

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Notification(models.Model):
+    NOTIF_TYPES = (
+        ('congratulations', 'congratulations'),
+        ('maintenance', 'maintenance'),
+        ('explore', 'explore'),
+    )
+
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    type = models.CharField(max_length=15, choices=NOTIF_TYPES)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
