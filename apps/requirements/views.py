0fc014f2c6a49bd1edfaf87704ebbf83af7ea412
Tolu Joel <toluisjoel@gmail.com>
2024-01-30 17:17:47 +0100

    chore: Rename `StateBillBoardRequirementListView` to`StateBillBoardReqDetailView`

--- apps/requirements/views.py
+++ apps/requirements/views.py
@@ -7,7 +7,7 @@ from .models import BillboardRequirement
 from .serializers import BillboardRequirementSerializer
 
 
-class StateBillBoardRequirementListView(ListAPIView):
+class StateBillBoardReqDetailView(ListAPIView):
     pagination_class = None
     serializer_class = BillboardRequirementSerializer
 
