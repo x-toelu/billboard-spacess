5d55105c5f3753e572f7872e691404f2f189ca9b
Tolu Joel <toluisjoel@gmail.com>
2024-01-30 14:20:18 +0100

    feat: Add `BillboardRequirement` model

--- apps/requirements/apps.py
+++ apps/requirements/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RequirementsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "requirements"
+    name = "apps.requirements"
