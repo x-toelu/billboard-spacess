07640dd84e55eef51b18441ce4567ae4d34912bb
Tolu Joel <toluisjoel@gmail.com>
2024-01-31 12:04:16 +0100

    chore: Update admin page for `Bookings` section

--- apps/bookings/admin.py
+++ apps/bookings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+
+class BookingAdmin(admin.ModelAdmin):
+    readonly_fields = ('amount',)
+    list_display = ['user', 'billboard', 'timeline', 'period', 'paid']
+    list_filter = ['paid', 'period']
+
+
+admin.site.register(Booking, BookingAdmin)
