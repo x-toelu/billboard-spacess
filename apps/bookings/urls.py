497e64e0ea7756d0ac4e93dbff549a96443ae086
Tolu Joel <toluisjoel@gmail.com>
2024-02-03 00:23:37 +0100

    feat: Implement payment on BillBoardBookingCreateView

--- apps/bookings/urls.py
+++ apps/bookings/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import BillBoardBookingCreateView
 
 urlpatterns = [
-    path('<int:billboard_id>/', BillBoardBookingCreateView.as_view(), name='billboards-list'),
+    path(
+        'pay/<int:billboard_id>/',
+        BillBoardBookingCreateView.as_view(), name='create-booking'
+    ),
 ]
--- apps/bookings/views.py
+++ apps/bookings/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.views import Response, status
 
 from apps.billboards.models import Billboard
+from services.paystack import PayStackSerivce
 
+from .models import Booking
 from .serializers import BillboardBookingSerializer
 
 
 class BillBoardBookingCreateView(CreateAPIView):
     serializer_class = BillboardBookingSerializer
 
-    def perform_create(self, serializer):
-        user = self.request.user
-        billboard_id = self.kwargs.get('billboard_id')
-        billboard = get_object_or_404(Billboard, id=billboard_id)
+    def create(self, request, *args, **kwargs):
+        billboard = get_object_or_404(Billboard, id=kwargs.get('billboard_id'))
+        booking = Booking(
+            user=request.user,
+            billboard=billboard,
+            **request.data,
+        )
 
-        return serializer.save(user=user, billboard=billboard)
+        paystack = PayStackSerivce()
+
+        pstack_data = paystack.initialise_payment(
+            booking.user.email,
+            str(booking.amount)
+        )
+
+        if pstack_data['status']:
+            booking.paystack_ref = pstack_data['data']['reference']
+            booking.save()
+
+            return Response(pstack_data['data'])
+
+        return Response(
+            {'status': False, 'message': 'Couldn\'t process payment, try again'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
