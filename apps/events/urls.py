9c6f436b4dd9dcea438fc7fe6b22363e4689b420
Tolu Joel <toluisjoel@gmail.com>
2024-02-07 14:41:05 +0100

    feat: Implement endpoint to retrieve event detail

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import EventListView
+from .views import EventDetailView, EventListView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events-list'),
+    path('<int:pk>/', EventDetailView.as_view(), name='events-detail'),
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Event
 from .serializers import EventSerializer
@@ -6,4 +6,8 @@ from .serializers import EventSerializer
 
 class EventListView(ListAPIView):
     queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventDetailView(RetrieveAPIView):
     serializer_class = EventSerializer
