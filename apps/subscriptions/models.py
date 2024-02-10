5709555199d99dc5e18117126f1d3092ba3b8b5e
Tolu Joel <toluisjoel@gmail.com>
2024-02-10 10:38:37 +0100

    feat: Add new methods to subscription model - has_expired - renew_subscription - _calculate_new_expiration

--- apps/subscriptions/models.py
+++ apps/subscriptions/models.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 from .choices import SubscriptionType
 
@@ -18,6 +21,31 @@ class Subscription(models.Model):
     expires_at = models.DateTimeField(null=True)
 
     is_active = models.BooleanField(default=True)
+
+    @property
+    def has_expired(self):
+        """
+        Checks if the subscription has expired.
+        """
+        return self.expires_at is not None and self.expires_at <= timezone.localtime()
+
+    def renew_subscription(self, duration):
+        """
+        Renews the subscription for the given duration.
+        """
+        new_expiration = self._calculate_new_expiration(duration)
+        self.expires_at = new_expiration
+        self.save()
+
+    def _calculate_new_expiration(self, duration):
+        """
+        Calculate new expiration date based
+        on current expiration date and given duration.
+        """
+        if self.expires_at:
+            return self.expires_at + timedelta(days=duration)
+        else:
+            return timezone.localtime() + timedelta(days=duration)
 
     def __str__(self):
         return f"{self.user}'s subscription ({self.plan})"
