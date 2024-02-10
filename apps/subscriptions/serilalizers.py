19382ec419d861158e3828b953e8bb51248b6ec8
Tolu Joel <toluisjoel@gmail.com>
2024-02-10 18:23:51 +0100

    chore: Add SubscriptionSerializer

---
+++ apps/subscriptions/serilalizers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Subscription
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = [
+            'user',
+            'plan',
+        ]
