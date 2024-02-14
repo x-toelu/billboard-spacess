6d5a0ed6d59ea12dddbaa60189868a95c623778c
Tolu Joel <toluisjoel@gmail.com>
2024-02-14 15:13:01 +0100

    feat: Implement endpoint to create subscription

--- apps/subscriptions/serilalizers.py
+++ apps/subscriptions/serilalizers.py
@@ -4,6 +4,7 @@ from .models import Subscription
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(read_only=True)
     class Meta:
         model = Subscription
         fields = [
