b61a55a241ad5290ac668a84a4b447f53ca385a5
Tolu Joel <toluisjoel@gmail.com>
2024-02-17 01:53:57 +0100

    feat: Implement endpoint to get new events

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import EventDetailView, EventListView
+from .views import EventDetailView, EventListView, GetNewEventsView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events-list'),
+    path('new/', GetNewEventsView.as_view(), name='new-events'),
     path('<int:pk>/', EventDetailView.as_view(), name='events-detail'),
 ]
