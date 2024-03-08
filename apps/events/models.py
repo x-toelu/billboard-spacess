2b4eba185072719044f413d561b1a4db94504eb1
Tolu Joel <toluisjoel@gmail.com>
2024-03-08 02:17:21 +0100

    refactor: Update image field for Event model

--- apps/events/models.py
+++ apps/events/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=225)
     description = models.TextField(null=True)
-    image = CloudinaryField()
+    image = models.URLField()
     start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.TimeField(null=True)
