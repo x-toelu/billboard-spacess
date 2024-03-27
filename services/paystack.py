cc2f070a3ffb66f7048f138b1aa9349688e0d911
Tolu Joel <toluisjoel@gmail.com>
2024-03-27 11:17:30 +0100

    refactor Update `verify_payment` method on `PayStackSerivce` for improved reliability and maintainability
    - Added support for configurable retry attempts and delay between retries.
    - Implemented a retry mechanism to handle transient failures gracefully.
    - Improved error handling and logging for better diagnostic information.

--- services/paystack.py
+++ services/paystack.py
@@ -1,8 +1,13 @@
+import logging
+import time
+
 import requests
 from django.conf import settings
 from django.utils import timezone
 
 from utils.constants import SUBSCRIBERS_FEATURES, SUBUNIT_CURRENCY
+
+logger = logging.getLogger(__name__)
 
 
 class PayStackSerivce:
@@ -26,17 +31,33 @@ class PayStackSerivce:
         response = self.session.post(url, headers=self.headers, json=payload)
         return response.json()
 
-    def verify_payment(self, paystack_ref):
+    def verify_payment(self, paystack_ref, max_retries=5, retry_delay=1):
         url = f"https://api.paystack.co/transaction/verify/{paystack_ref}"
-        response = self.session.get(url, headers=self.headers)
 
-        while True:
-            if response.json()["data"]["status"] == "success":
+        for _ in range(max_retries):
+            response = self.session.get(url, headers=self.headers)
+
+            if response.status_code != 200:
+                logger.error(
+                    f"HTTP request failed with status code: {response.status_code}")
+                return False
+
+            try:
+                status = response.json()["data"]["status"]
+            except (KeyError, ValueError):
+                logger.error(
+                    "Failed to parse JSON response or missing required data.")
+                return False
+
+            if status == "success":
                 return True
-            elif response.json()["data"]["status"] in ["ongoing", "pending", "processing", "queued"]:
-                continue
+            elif status in ["ongoing", "pending", "processing", "queued"]:
+                time.sleep(retry_delay)
             else:
                 return False
+
+        logger.warning("Max retries reached without successful verification.")
+        return False
 
     def create_subscription(self, email: str, plan: str):
         url = "https://api.paystack.co/subscription"
