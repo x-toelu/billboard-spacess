8a9619d5b144972ad412a463e3aa60fd8924a2a6
Tolu Joel <toluisjoel@gmail.com>
2024-03-30 08:30:35 +0100

    feat: Add logic to handle duplicate subscription

--- apps/subscriptions/views.py
+++ apps/subscriptions/views.py
@@ -15,7 +15,7 @@ class SubscriptionView(GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
+        subscription_plan = serializer.validated_data.get('plan')
 
         user = request.user
         subscription = Subscription.objects.get(user=user)
@@ -23,14 +23,27 @@ class SubscriptionView(GenericAPIView):
         paystack = PayStackSerivce()
         pstack_data = paystack.create_subscription(
             user.email,
-            validated_data.get('plan')
+            subscription_plan
         )
 
-        if pstack_data['status']:
+        if pstack_data['status'] and pstack_data['condition'] == "paid":
             subscription.paystack_sub_code = pstack_data['data']['subscription_code']
             subscription.paystack_email_token = pstack_data['data']['email_token']
+            subscription.plan = subscription_plan
+            subscription.is_active = True
             subscription.save()
-            return Response({'message': "Billed successfully"})
+            return Response({'condition': 'paid', 'message': "Subscription successful"})
+
+        elif pstack_data['status'] and pstack_data['condition'] == "verify":
+            subscription.paystack_ref = pstack_data['data']['reference']
+            subscription.plan = subscription_plan
+            subscription.is_active = False
+            subscription.save()
+            return Response({'id': subscription.id, **pstack_data})
+
+        # handle duplicate subscription
+        elif not pstack_data['status'] and pstack_data['condition'] == "paid":
+            return Response({'condition': 'paid', 'message': "Subscription had previously been paid"})
 
         return Response(
             {'status': False, 'message': 'Couldn\'t process payment, try again'},
