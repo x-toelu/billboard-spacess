cc67bf57b48a00ea1839a5e62c1bd4904388f960
Tolu Joel <toluisjoel@gmail.com>
2024-03-30 16:56:11 +0100

    refactor: Modify code for subscription models

--- apps/subscriptions/permissions.py
+++ apps/subscriptions/permissions.py
@@ -4,17 +4,14 @@ from apps.subscriptions.models import Subscription
 
 
 def has_active_subscription(user):
-    subscription: Subscription = user.subscription
-    if subscription and subscription.is_active:
-        return not subscription.has_expired
-    return False
+    sub: Subscription = user.subscription
+    return not sub.has_expired if sub and sub.is_active else False
 
 
 class IsBasicOrProPlanUser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         sub: Subscription = user.subscription
-
         return bool(has_active_subscription(user) and sub.plan in ['basic', 'pro'])
 
 
@@ -22,5 +19,4 @@ class IsProPlanUser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         sub: Subscription = user.subscription
-
         return bool(has_active_subscription(user) and sub.plan == 'pro')
