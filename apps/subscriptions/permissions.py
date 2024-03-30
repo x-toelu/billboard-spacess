01f167e21a74d3dbcf5207ebb39fc5ad9be0c405
Tolu Joel <toluisjoel@gmail.com>
2024-03-30 16:35:11 +0100

    refactor: Update subscription permission classes

--- apps/subscriptions/permissions.py
+++ apps/subscriptions/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework.permissions import BasePermission
 
+from apps.subscriptions.models import Subscription
+
 
 def has_active_subscription(user):
-    subscription = user.subscription
-    if subscription:
+    subscription: Subscription = user.subscription
+    if subscription and subscription.is_active:
         return not subscription.has_expired
     return False
 
@@ -11,18 +13,14 @@ def has_active_subscription(user):
 class IsBasicOrProPlanUser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
+        sub: Subscription = user.subscription
 
-        if has_active_subscription(user):
-            if user.subscription.plan in ['basic', 'pro']:
-                return True
-        return False
+        return bool(has_active_subscription(user) and sub.plan in ['basic', 'pro'])
 
 
 class IsProPlanUser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
+        sub: Subscription = user.subscription
 
-        if has_active_subscription(user):
-            if user.subscription.plan == 'pro':
-                return True
-        return False
+        return bool(has_active_subscription(user) and sub.plan == 'pro')
